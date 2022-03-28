8d9dff03ec45c00baa4e41c1fc82bcaaad51ec64
bea <chamoischamois@protonmail.com>
2022-03-28 23:07:59 +0800

    Fixed temperature display and added basic styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+import './style.css'
 import { getWeather } from './API.js'
-import { displayWeather } from './updateDisplay.js'
+import { displayWeather, toggleCelsius } from './updateDisplay.js'
 
 const searchBtn = document.getElementById('search-btn')
 const searchInput = document.getElementById('search-box')
+const tempToggle = document.getElementById('temp-box')
 
 searchBtn.addEventListener('click', () => {
   getWeather(searchInput.value).then(result => {
     displayWeather(result)
   })
+})
+
+tempToggle.addEventListener('click', toggleCelsius)
+
+getWeather('Iceland').then(result => {
+  displayWeather(result)
 })
--- src/updateDisplay.js
+++ src/updateDisplay.js
@@ -1,12 +1,26 @@
+import snow from './icons/snowflake.png'
 const countryName = document.getElementById('country')
 const weatherType = document.getElementById('weather')
 const temperature = document.getElementById('temperature')
+const degreeToggle = document.getElementById('degree-toggle')
+const icon = document.getElementById('weather-icon')
 
 function displayWeather (country) {
   // eslint-disable-next-line dot-notation
   countryName.textContent = country.name
-  temperature.textContent = `${country.main.temp} °F`
+  temperature.textContent = country.main.temp
   weatherType.textContent = country.weather[0].description
 }
+function toggleCelsius () {
+  if (degreeToggle.textContent === '°F') {
+    degreeToggle.textContent = '°C'
+    const tempVal = ((parseFloat(temperature.textContent) - 32) * (5 / 9)).toFixed(2)
+    temperature.textContent = tempVal
+  } else {
+    degreeToggle.textContent = '°F'
+    const tempVal = ((parseFloat(temperature.textContent) * (9 / 5) + 32)).toFixed(2)
+    temperature.textContent = tempVal
+  }
+}
 
-export { displayWeather }
+export { displayWeather, toggleCelsius }
