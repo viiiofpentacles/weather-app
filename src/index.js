ff016317617b3ccf78b54561c62563f5bad9dcf4
bea <chamoischamois@protonmail.com>
2022-03-29 15:50:12 +0800

    Added weather and search icons

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 import './style.css'
 import { getWeather } from './API.js'
-import { displayWeather, toggleCelsius } from './updateDisplay.js'
+import { displayWeather, toggleCelsius, updateIcon, displayError } from './updateDisplay.js'
+import searchIcon from './icons/magnifying-glass.png'
 
 const searchBtn = document.getElementById('search-btn')
+const searchImg = document.getElementById('search-icon')
 const searchInput = document.getElementById('search-box')
 const tempToggle = document.getElementById('temp-box')
 const unitInput = document.getElementById('units')
 
-searchBtn.addEventListener('click', () => {
+searchImg.src = searchIcon
+
+function weatherForecast () {
   getWeather(searchInput.value, unitInput.value).then(result => {
-    displayWeather(result)
+    if (result.name) {
+      displayWeather(result)
+      updateIcon(result)
+    } else {
+      displayError()
+    }
   })
+}
+
+searchBtn.addEventListener('click', weatherForecast)
+
+searchInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    weatherForecast()
+  }
 })
 
 tempToggle.addEventListener('click', toggleCelsius)
 
 getWeather('Iceland', 'imperial').then(result => {
-  displayWeather(result)
+  if (result.name) {
+    displayWeather(result)
+    updateIcon(result)
+  } else {
+    displayError()
+  }
 })
--- src/updateDisplay.js
+++ src/updateDisplay.js
@@ -1,10 +1,17 @@
+import storm from './icons/storm.png'
+import drizzle from './icons/drizzle.png'
+import rain from './icons/rain.png'
 import snow from './icons/snowflake.png'
+import atmosphere from './icons/fog.png'
+import clear from './icons/sun.png'
+import clouds from './icons/sunny.png'
 const countryName = document.getElementById('country')
 const weatherType = document.getElementById('weather')
 const temperature = document.getElementById('temperature')
 const degreeToggle = document.getElementById('degree-toggle')
 const unitInput = document.getElementById('units')
 const icon = document.getElementById('weather-icon')
+const container = document.getElementById('container')
 
 function displayWeather (country) {
   // eslint-disable-next-line dot-notation
@@ -12,6 +19,7 @@ function displayWeather (country) {
   temperature.textContent = country.main.temp
   weatherType.textContent = country.weather[0].description
 }
+
 function toggleCelsius () {
   if (degreeToggle.textContent === '°F') {
     degreeToggle.textContent = '°C'
@@ -26,4 +34,40 @@ function toggleCelsius () {
   }
 }
 
-export { displayWeather, toggleCelsius }
+function updateIcon (country) {
+  if (String(country.weather[0].id).charAt(0) === '2') {
+    icon.src = storm
+    container.classList.remove(container.classList.item(0))
+    container.classList.add('storm')
+  } else if (String(country.weather[0].id).charAt(0) === '3') {
+    icon.src = drizzle
+    container.classList.remove(container.classList.item(0))
+    container.classList.add('drizzle')
+  } else if (String(country.weather[0].id).charAt(0) === '5') {
+    icon.src = rain
+    container.classList.remove(container.classList.item(0))
+    container.classList.add('rain')
+  } else if (String(country.weather[0].id).charAt(0) === '6') {
+    icon.src = snow
+    container.classList.remove(container.classList.item(0))
+    container.classList.add('snow')
+  } else if (String(country.weather[0].id).charAt(0) === '7') {
+    icon.src = atmosphere
+    container.classList.remove(container.classList.item(0))
+    container.classList.add('atmosphere')
+  } else if (String(country.weather[0].id) === '800') {
+    icon.src = clear
+    container.classList.remove(container.classList.item(0))
+    container.classList.add('clear')
+  } else if (String(country.weather[0].id) !== '800' && String(country.weather[0].id).charAt(0) === '8') {
+    icon.src = clouds
+    container.classList.remove(container.classList.item(0))
+    container.classList.add('clouds')
+  }
+}
+
+function displayError () {
+  countryName.textContent = 'Please check that you have entered the name correctly.'
+}
+
+export { displayWeather, toggleCelsius, updateIcon, displayError }
